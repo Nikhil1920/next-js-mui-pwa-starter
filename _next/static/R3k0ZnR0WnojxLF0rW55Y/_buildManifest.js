239f5ca1a08ed852df28f0e89982e4ce5dbfee35
Nikhil1920 <57902888+Nikhil1920@users.noreply.github.com>
2022-04-07 14:55:51 +0000

    Deploying to gh-pages from @ Nikhil1920/next-js-mui-pwa-starter@b3b21f42f74ba7ae4b30c0a730fc75412da2beea 🚀

--- _next/static/R3k0ZnR0WnojxLF0rW55Y/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/741-7efecac28fbd188b.js","static/css/149b18973e5508c7.css","static/chunks/pages/index-e334b1c8ca68c0f1.js"],"/_error":["static/chunks/pages/_error-4ec6510e25a99a3c.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
