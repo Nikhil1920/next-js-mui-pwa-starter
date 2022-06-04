88569fa0dc743b73fa496366a2ad900dd401ace5
Nikhil Reddy Avuthu <anr.2648@gmail.com>
2022-06-04 12:28:12 +0530

    use path to assets relative to root

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,12 +21,12 @@ const App = ({ Component, pageProps }: AppProps) => {
     return (
         <>
             <Head>
-                <link rel="icon" href="favicon.ico" />
-                <link rel="manifest" href="manifest.json" />
+                <link rel="icon" href="/favicon.ico" />
+                <link rel="manifest" href="/manifest.json" />
                 <link
                     rel="apple-touch-icon"
                     sizes="192x192"
-                    href="icons/icon-192x192.png"
+                    href="/icons/icon-192x192.png"
                 />
 
                 <meta name="theme-color" content="#1976d2" />
