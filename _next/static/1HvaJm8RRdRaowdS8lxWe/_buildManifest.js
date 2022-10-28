bd45b5124eda6eafd884180632c629ba8afad0ca
Nikhil1920 <57902888+Nikhil1920@users.noreply.github.com>
2022-10-28 05:15:08 +0000

    Deploying to gh-pages from @ Nikhil1920/next-js-mui-pwa-starter@ca38e488dee0071d8b942273ddcb20e7feebfbeb 🚀

---
+++ _next/static/1HvaJm8RRdRaowdS8lxWe/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/698-a20d85975e9ec436.js","static/chunks/pages/index-cc5638a2874e939f.js"],"/_error":["static/chunks/pages/_error-b5a83d78c78e1f32.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/kMas7U-n-RomiT1qpIEoI/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/698-a20d85975e9ec436.js","static/chunks/pages/index-e89c12c7ee96276a.js"],"/_error":["static/chunks/pages/_error-b5a83d78c78e1f32.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
